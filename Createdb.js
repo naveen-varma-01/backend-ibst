360f2df5f74a6fbb2b4f319938738c6945c5cf9d
naveen <naveenvarma0104@gmail.com>
2025-05-27 19:32:26 +0530

    final

--- Createdb.js
+++ Createdb.js
@@ -27,6 +27,46 @@ const hostels = [
     pricePerNight: 45,
     imageUrl: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg"
   },
+  {
+    name: "La Tamara",
+    city: "Hyderabad",
+    description: "High class and Luxury rooms with city view.",
+    averageRating: 4.7,
+    pricePerNight: 45,
+    imageUrl: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg"
+  },
+  {
+    name: "Paradise",
+    city: "Banglore",
+    description: "Affordable and cozy hostel.",
+    averageRating: 4.5,
+    pricePerNight: 30,
+    imageUrl: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg"
+  },
+  {
+    name: "Le Meridian",
+    city: "Hyderabad",
+    description: "Clean and neat friendly hostel.",
+    averageRating: 4.5,
+    pricePerNight: 30,
+    imageUrl: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg"
+  },
+  {
+    name: "Sydney Rooms",
+    city: "Sydney",
+    description: "Famous for tourists and Qualified staff.",
+    averageRating: 4.5,
+    pricePerNight: 30,
+    imageUrl: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg"
+  },
+  {
+    name: "BoxRooms",
+    city: "London",
+    description: "High class and Luxury rooms with city view.",
+    averageRating: 4.5,
+    pricePerNight: 30,
+    imageUrl: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg"
+  },
 ];
 
 hostels.forEach(h => {
